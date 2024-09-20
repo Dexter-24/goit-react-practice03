83fd878b494e2c74d469d163fc6d811a6fe8ac14
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-09-20 22:49:26 +0300

    task3

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,5 @@
 export const selectBaseCurrency = state => state.currency.baseCurrency;
+
+export const selectIsLoading = state => state.currency.loading;
+export const selectError = state => state.currency.error;
+export const selectExchangeInfo = state => state.currency.exchangeInfo;
