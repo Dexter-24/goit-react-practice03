9bc02f850a4300af772a628ce7d5d1d949021afd
Yana17-ratsa <ratsayana13@gmail.com>
2024-10-04 20:30:09 +0300

    4oct 1

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,3 +3,4 @@ export const selectBaseCurrency = state => state.currency.baseCurrency;
 export const selectIsLoading = state => state.currency.loading;
 export const selectError = state => state.currency.error;
 export const selectExchangeInfo = state => state.currency.exchangeInfo;
+export const selectRates = state => state.currency.rates;
